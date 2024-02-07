cb233a64da670980c21b2c6a97b1988d1cd27a6e
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-02-07 23:59:10 +0600

    some issue solved in task 3

--- t3.js
+++ t3.js
@@ -2,7 +2,7 @@ let myArray = [1 , null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }];
 function deleteInvalids(para){
     let allNum = [];
     for (let item of para) {
-        if(typeof item === 'number'){
+        if(typeof item === 'number' &&  isNaN(item) === false ){
             allNum.push(item);
         }
     }
